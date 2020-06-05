d18fbfec5cef65c86b5305283330e9c1406408ad
alouzao10 <alouzao10@gmail.com>
2020-06-05 11:19:30 -0400

    Small updates

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { v1 as uuid } from 'uuid';
+import './style.css';
 
-function Projects() {
-  return (
-    <div>
-      <h1>My Projects...</h1>
-    </div>
-  );
+class Projects extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [
+        {
+          name: 'FIMAN',
+          description:
+            'Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...Some info about FIMAN...',
+          id: uuid(),
+        },
+        {
+          name: 'SERA',
+          description:
+            'Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...Some info about SERA...',
+          id: uuid(),
+        },
+        {
+          name: 'SRMP',
+          description:
+            'Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...Some info about SRMP...',
+          id: uuid(),
+        },
+        {
+          name: 'HERMP',
+          description:
+            'Some info about HERMP...Some info about HERMP...Some info about HERMP...Some info about HERMP...Some info about HERMP...Some info about HERMP...Some info about HERMP...Some info about HERMP...Some info about HERMP...Some info about HERMP...Some info about HERMP...Some info about HERMP...Some info about HERMP...',
+          id: uuid(),
+        },
+        {
+          name: 'CFRMP',
+          description:
+            'Some info about CFRMP...Some info about CFRMP...Some info about CFRMP...Some info about CFRMP...Some info about CFRMP...Some info about CFRMP...Some info about CFRMP...Some info about CFRMP...Some info about CFRMP...',
+          id: uuid(),
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className='projects'>
+        <h3 className='header'>Current Project Work</h3>
+        {this.state.projects.map((project) => (
+          <Fragment>
+            <h4 className='projectTitle'>{project.name}</h4>
+            <p className='projectDescription'>{project.description}</p>
+          </Fragment>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Projects;
--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.css';
+import logo from '../../logo.svg';
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <img src={logo} className='image' />
     </div>
   );
 }
